206d52c2a16049be72fc9727d767975d532544a7
Jose Benitez <jose@intuitivo.ai>
2022-10-30 20:23:35 -0300

    edit footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <div className="max-w-6xl mx-auto px-5 p-10">
         <h3 className="font-semibold text-2xl mb-10 lg:mb-0 lg:pr-4">
-          Sample website built with{' '}
+          Website built with{' '}
           <a className="underline" href="https://outstatic.com/">
             Outstatic
           </a>{' '}
