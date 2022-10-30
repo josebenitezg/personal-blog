7d76b755e2e8ddd172c145fa291ae7e49733c8f5
Jose Benitez <jose@intuitivo.ai>
2022-10-30 20:39:02 -0300

    modify footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,7 @@ const Footer = () => {
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <div className="max-w-6xl mx-auto px-5 p-10">
         <h3 className="font-semibold text-2xl mb-10 lg:mb-0 lg:pr-4">
-          Website built with{' '}
-          <a className="underline" href="https://outstatic.com/">
-            Outstatic
-          </a>{' '}
-          and{' '}
-          <a className="underline" href="https://nextjs.org/">
-            Next.js
-          </a>
+        “Insanity is doing the same thing over and over and expecting different results.” – Albert Einstein
         </h3>
       </div>
     </footer>
